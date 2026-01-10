ebf2f79bcb7f90ab9b10f85766dae00bc101d082
Daniel <da@gmail.com>
2026-01-10 10:03:11 -0600

    Cambios 2

--- CursoAPis.MinimalApi/obj/Debug/net10.0/CursoAPis.MinimalApi.AssemblyInfo.cs
+++ CursoAPis.MinimalApi/obj/Debug/net10.0/CursoAPis.MinimalApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoAPis.MinimalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4a3d8ac9520a1f19ad86399abada0f1e3588aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoAPis.MinimalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoAPis.MinimalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
